311872d24df3c0f4ffbc28915cc88b4a4ab8be6c
safroniv <safroniv@mail.ru>
2022-07-01 13:56:05 +0300

    GB - bootCamp Python Testing

---
+++ main.py
@@ -0,0 +1,12 @@
+def print_hi(name):
+
+    print(f'Hi, {name}')
+    n = 10
+    print(f"Переменная n = {n}")
+    m=n
+    print(f"Сейчас m = n = {m}")
+    m=12
+    print(f"А сейчас m = {m}")
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
